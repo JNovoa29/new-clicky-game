a7f21084d23363b5cc617d2b3efeed5d1c428e27
jnovoa29 <jordan.novoa15@gmail.com>
2018-06-05 19:36:39 -0700

    added return navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Navbar = props => {
+    return (
     <div>
     <nav class="navbar">
         <span class="navbar-brand mb-0 h1">Overwatch Match Game</span>
     </nav>
 </div>
+    )
 }
 
 
